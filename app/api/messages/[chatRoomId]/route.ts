ffb1490a5043ef2ccc106970676b502ee0b9343b
Gideon <golaiya123@gmail.com>
2025-03-27 18:46:46 +0100

    fixed dynamic route deployment issue

--- app/api/messages/[chatRoomId]/route.ts
+++ app/api/messages/[chatRoomId]/route.ts
@@ -2,11 +2,11 @@ import { connectToDB } from "@/utils/database";
 import Message from "@/model/messages";
 import { NextRequest } from "next/server";
 
-export const GET = async ( request: NextRequest, { params }: { params: { chatRoomId: string }}) => {
+export const GET = async ( request: NextRequest, { params }: { params: Promise<{ chatRoomId: string }> }) => {
     try {
         await connectToDB()
 
-        const { chatRoomId } = params
+        const { chatRoomId } = await params
         const getChatRoomMessages = await Message.find({ chatRoomId }).populate('senderId', 'username avatar');
         
         return new Response(JSON.stringify(getChatRoomMessages), { status: 200 })
