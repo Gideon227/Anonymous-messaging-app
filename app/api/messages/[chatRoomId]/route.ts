7ef71bb4d27ca095dcc22ed9195c1e62a6b6faa2
Gideon <golaiya123@gmail.com>
2025-03-27 18:24:14 +0100

    redeployment issues

--- app/api/messages/[chatRoomId]/route.ts
+++ app/api/messages/[chatRoomId]/route.ts
@@ -1,12 +1,13 @@
 import { connectToDB } from "@/utils/database";
 import Message from "@/model/messages";
+import { NextRequest } from "next/server";
 
-export const GET = async ( request: Request, { params }: { params: { chatRoomId: string }}) => {
+export const GET = async ( request: NextRequest, { params }: { params: { chatRoomId: string }}) => {
     try {
         await connectToDB()
 
-        const { chatRoomId } = await params
-        const getChatRoomMessages = await Message.find({ chatRoomId: chatRoomId }).populate('senderId', 'username avatar');
+        const { chatRoomId } = params
+        const getChatRoomMessages = await Message.find({ chatRoomId }).populate('senderId', 'username avatar');
         
         return new Response(JSON.stringify(getChatRoomMessages), { status: 200 })
     } catch (error) {
